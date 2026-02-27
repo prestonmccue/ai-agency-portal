35f30cacd9dbc25db13e4cc7afe88216bb5cdb17
Preston McCue <prestonmccue@Prestons-Mini.lan>
2026-02-27 14:46:20 -0800

    fix: restore clerkMiddleware for auth() to work

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export function middleware() {
-  return NextResponse.next();
-}
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!_next|.*\\..*).*)'],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  ],
 };
